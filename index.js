1d0a43b382455682424cb6d5eaae3672358b2c01
par jin sung <parkgutime@gmail.com>
2023-07-07 13:28:29 +0900

    [add] 네이버 maps 위치값 주소 변환

--- index.js
+++ index.js
@@ -15,35 +15,34 @@ app.listen(port, function () {
 // get으로 페이지 생성
 // 해당 페이지로 접속 했을 때 어떤 데이터를 보낼 지도 결정
 app.get("/pharmacy_list", (req, res) => {
-  let response = null;
-  try {
-    let api = async () => {
+  let api = async () => {
+    let response = null;
+    try {
       response = await axios.get(
         "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire",
         {
           params: {
             serviceKey:
-              "vaAR8EEsvULJ%2BVngbDQLYQWlAJJhEn55j20aVKvSKORy9Nz62MTOaHkQNs6T3Zkg9rQ1SV8FAWwnSAMrCqI4tg%3D%3D",
-            Q0: "서울특별시",
-            Q1: "강남구",
-            QT: "1",
-            QN: "",
-            QRD: "",
-            pageNo: "1",
-            numOfRows: "1000",
+              "vaAR8EEsvULJ+VngbDQLYQWlAJJhEn55j20aVKvSKORy9Nz62MTOaHkQNs6T3Zkg9rQ1SV8FAWwnSAMrCqI4tg==",
+            Q0: req.query.Q0,
+            Q1: req.query.Q1,
+            QT: req.query.QT,
+            QN: req.query.QN,
+            QRD: req.query.QRD,
+            pageNo: req.query.pageNo,
+            numOfRows: req.query.numOfRows,
           },
         }
       );
-    };
-    api().then((response) => {
-      // 보내는 내용일때 res
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      //response에 속해있는 위에 결과물들을 json 형태로 전달해주는 것
-      res.json(response.data.response.body);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-
-  return response;
+    } catch (e) {
+      console.log(e);
+    }
+    return response;
+  };
+  api().then((response) => {
+    // 보내는 내용일때 res
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    //response에 속해있는 위에 결과물들을 json 형태로 전달해주는 것
+    res.json(response.data.response.body);
+  });
 });
