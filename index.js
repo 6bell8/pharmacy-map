0117319a4c226f7d5004805297df7fba3c2d9874
par jin sung <parkgutime@gmail.com>
2023-07-18 11:21:18 +0900

    [add] 내 주변 약국 완성

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ let express = require("express");
 let axios = require("axios");
 
 let app = express();
-let = port = process.env.PORT || 8080;
+let port = process.env.PORT || 8080;
 
 app.use(express.static("public_html"));
 
 app.listen(port, function () {
   console.log(`${port} server 시작 됨`);
 });
-
-// https://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire?serviceKey=vaAR8EEsvULJ%2BVngbDQLYQWlAJJhEn55j20aVKvSKORy9Nz62MTOaHkQNs6T3Zkg9rQ1SV8FAWwnSAMrCqI4tg%3D%3D&Q0=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C&Q1=%EA%B0%95%EB%82%A8%EA%B5%AC&QT=1&QN=%EC%82%BC%EC%84%B1%EC%95%BD%EA%B5%AD&ORD=NAME&pageNo=1&numOfRows=10
 
 // get으로 페이지 생성
 // 해당 페이지로 접속 했을 때 어떤 데이터를 보낼 지도 결정
